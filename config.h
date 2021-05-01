ee37a07c9d9c416ca0734782e693a15784cd03e5
Rishav Singh <rsh04613@gmail.com>
2021-05-01 11:49:44 +0530

    Removed swallow patch

--- config.h
+++ config.h
@@ -48,14 +48,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	*/
-	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
-	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
-	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
-	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
-	{ "Emulator", NULL,       NULL,       		0,            1,           0,		  0,        -1 },
-	{ "trayer",   NULL,       NULL,       		0,            0,           1,		  0,        -1 },
+	/* class    instance      title       	 tags mask    isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           -1 },
+	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           -1 },
+	{ NULL,       NULL,       "Event Tester",   0,            0,           -1 },
+	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           -1 },
+	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           -1 },
+	{ "Emulator", NULL,       NULL,       		0,            1,           -1 },
+	{ "trayer",   NULL,       NULL,       		0,            0,           -1 },
 };
 
 /* layout(s) */
