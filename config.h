eb4d9235f0ebb0848e81dccd2c1bc8bb6e70ac64
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-08-02 02:48:49 +0530

    Fonts and file manager bindings changed

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* Constants */
 #define TERMINAL "alacritty"
+#define BROWSER "brave"
 #define TERMCLASS "St"
 
 /* appearance */
@@ -21,7 +22,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=14", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
+static char *fonts[]          = { "NimbusSans:size=16", "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -167,11 +168,11 @@ static Key keys[] = {
 	/* { MODKEY|ShiftMask,		XK_Tab,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_q,		killclient,	{0} },
 	{ MODKEY|ShiftMask,	XK_q,		spawn,		SHCMD("sysact") },
-	{ MODKEY,			XK_c,		spawn,		SHCMD("$BROWSER") },
+	{ MODKEY,			XK_c,		spawn,		SHCMD(BROWSER) },
 	{ MODKEY,			XK_w,		spawn,		SHCMD("wall_nitrogen") },
 	{ MODKEY|ShiftMask,	XK_w,		spawn,		SHCMD(TERMINAL " -e sudo nmtui") },
-	{ MODKEY,			XK_e,		spawn,		SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") },
-	{ MODKEY|ShiftMask,	XK_e,		spawn,		SHCMD("pcmanfm") },
+	{ MODKEY,			XK_e,		spawn,		SHCMD("pcmanfm") },
+	{ MODKEY|ShiftMask,	XK_e,		spawn,		SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD("skippy-xd-runner") },
 	/* { MODKEY|ShiftMask,	XK_e,		spawn,		SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") }, */
 	{ MODKEY,			XK_r,		spawn,		SHCMD(TERMINAL " -e ranger") },
@@ -253,8 +254,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY|ShiftMask,	XK_space,	togglefloating,	{0} },
 
-	{ 0,				XK_Print,	spawn,		SHCMD("scrot '%Y:%m:%d:%H:%M:%S_scrot.png' -e 'mv $f ~/Pictures/'") },
-	{ ShiftMask,		XK_Print,	spawn,		SHCMD("scrot -s '%Y:%m:%d:%H:%M:%S_scrot.png' -e 'mv $f ~/Pictures/'") },
+	{ 0,				XK_Print,	spawn,		SHCMD("maimpick") },
+	{ ShiftMask,			XK_Print,	spawn,		SHCMD("maimpick") },
 	/* { MODKEY,			XK_Print,	spawn,		SHCMD("dmenurecord") }, */
 	/* { MODKEY|ShiftMask,		XK_Print,	spawn,		SHCMD("dmenurecord kill") }, */
 	/* { MODKEY,			XK_Delete,	spawn,		SHCMD("dmenurecord kill") }, */
@@ -274,7 +275,7 @@ static Key keys[] = {
 	{ 0, XF86XK_PowerOff,		spawn,		SHCMD("sysact") },
 	{ 0, XF86XK_Calculator,		spawn,		SHCMD(TERMINAL " -e bc -l") },
 	{ 0, XF86XK_Sleep,		spawn,		SHCMD("sudo -A zzz") },
-	{ 0, XF86XK_WWW,		spawn,		SHCMD("$BROWSER") },
+	{ 0, XF86XK_WWW,		spawn,		SHCMD(BROWSER) },
 	{ 0, XF86XK_DOS,		spawn,		SHCMD(TERMINAL) },
 	{ 0, XF86XK_ScreenSaver,	spawn,		SHCMD("slock & xset dpms force off; mpc pause; pauseallmpv") },
 	{ 0, XF86XK_TaskPane,		spawn,		SHCMD(TERMINAL " -e htop") },
